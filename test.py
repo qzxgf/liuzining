6da284eed4de03326cb4bc91e8e5358220ef0c32
lzn <lzn@qq.com>
2021-07-06 23:35:04 +0800

    thrid

--- test.py
+++ test.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python
 print("hello")
 print("1236")
-print("1236")
-print("1236")
-print("1236")
-print("1236")
-print("1236")
+print("12dasfkjh alksdfjaso;lkdfj a;sldkfj36")
 print("1236")
